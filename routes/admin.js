2815a54d49e7dbd8f23dab42d8da64269edad974
Warnsley <Marcus.Warnsley@navient.com>
2016-04-16 22:00:29 -0400

    Adding more structure to te handlebars files and adding the foundation css for structure

--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next){
 	res.render('admin/index');
 });
 
+router.get('/add', function(req, res, next){
+	res.render('admin/add');
+});
+
 module.exports = router;
